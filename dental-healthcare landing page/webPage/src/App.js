972245410d28063261aa8a9bfc8eebd0fa0c85eb
Ahmed6010 <ahmed15151@hotmail.fr>
2022-12-31 16:08:22 +0100

    App component

---
+++ dental-healthcare landing page/webPage/src/App.js
@@ -0,0 +1,16 @@
+import { React } from 'react'
+import LandingPage from './Components/landingPage'
+
+
+const App = () => {
+
+  return (
+    <div className={`w-screen h-screen mt-0`}>   
+      <LandingPage/>
+    </div>
+  )
+}
+
+export default App
+
+// <></>
